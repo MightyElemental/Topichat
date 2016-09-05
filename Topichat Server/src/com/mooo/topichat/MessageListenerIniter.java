ea45d4e5d1f49ca51b53eed566a42647b5144edb
James <minebox30@gmail.com>
2016-09-05 21:30:01 +0100

    Added addListener() method & added documentation

--- Topichat Server/src/com/mooo/topichat/MessageListenerIniter.java
+++ Topichat Server/src/com/mooo/topichat/MessageListenerIniter.java
@@ -9,10 +9,22 @@ public class MessageListenerIniter {
 	public static List<MessageListener> listeners = new ArrayList<MessageListener>();
 	
 	/** This method should be invoked when a web-client send a message<br>
-	 * This method passes the information on to the listeners */
-	public static void onMessageRecived(String UID, String dir, String message) {
+	 * This method passes the information on to the listeners
+	 * 
+	 * @param uid
+	 *            the UID of the client that sent the message
+	 * @param dir
+	 *            the directory of the chat
+	 * @param message
+	 *            the message sent to the server */
+	public static void onMessageRecived(String uid, String dir, String message) {
 		for (MessageListener l : listeners) {
-			l.onMessageRecived(UID, dir, message);
+			l.onMessageRecived(uid, dir, message);
 		}
+	}
+	
+	/** Use to add a message listener */
+	public static void addListener(MessageListener ml) {
+		listeners.add(ml);
 	}
 }
